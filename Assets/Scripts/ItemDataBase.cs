79cb32562f90b63bf43e3f7858b8df28e8dd5a2d
DESKTOP-GFI8O0F\有田翔悟 <aritashogo@gmail.com>
2020-02-17 00:38:09 +0900

    0216  マスクを使い、初森へ来たときのイベントシーンを、ラフで追加してみた。

--- Assets/Scripts/ItemDataBase.cs
+++ Assets/Scripts/ItemDataBase.cs
@@ -60,6 +60,7 @@ public class ItemDataBase : SingletonMonoBehaviour<ItemDataBase>
     private string _koyutp;
 
     private int _itemhyouji;
+    private int _ex_kaisu;
 
     private int i;
     private int count;
@@ -87,6 +88,8 @@ public class ItemDataBase : SingletonMonoBehaviour<ItemDataBase>
 
         trash_ID_1 = 500;
         sheet_no = 0;
+
+        _ex_kaisu = 1;
 
         while (sheet_no < excel_itemdatabase.sheets.Count)
         {
@@ -149,7 +152,7 @@ public class ItemDataBase : SingletonMonoBehaviour<ItemDataBase>
                 //ここでリストに追加している
                 items.Add(new Item(_id, _file_name, _name, _name_hyouji, _desc, _hp, _day, _quality, _exp, _ex_probability,
                     _rich, _sweat, _bitter, _sour, _crispy, _fluffy, _smooth, _hardness, _jiggly, _chewy, _powdery, _oily, _watery, _type, _subtype, _girl1_like, 
-                    _cost, _sell, _tp01, _tp02, _tp03, _tp04, _tp05, _tp06, _tp07, _tp08, _tp09, _tp10, _koyutp, 0, 3, _itemhyouji));
+                    _cost, _sell, _tp01, _tp02, _tp03, _tp04, _tp05, _tp06, _tp07, _tp08, _tp09, _tp10, _koyutp, 0, _ex_kaisu, _itemhyouji));
 
                 ++count;
             }
@@ -164,7 +167,7 @@ public class ItemDataBase : SingletonMonoBehaviour<ItemDataBase>
 
                 for (i = 0; i < excel_itemdatabase.sheets[sheet_no].list[0].ItemID - sheet_count; i++) //次のシートの0行目のID番号をみる。例えば300とか。
                 {
-                    items.Add(new Item(_id+i+1, "orange", "empty", "", "", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Non", "Non", 0, 0, 0, "Non", "Non", "Non", "Non", "Non", "Non", "Non", "Non", "Non", "Non", "Non", 0, 3, 0));
+                    items.Add(new Item(_id+i+1, "orange", "empty", "", "", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Non", "Non", 0, 0, 0, "Non", "Non", "Non", "Non", "Non", "Non", "Non", "Non", "Non", "Non", "Non", 0, _ex_kaisu, 0));
                 }
 
                 sheet_topendID.Add(excel_itemdatabase.sheets[sheet_no].list[0].ItemID); // 次sheetの頭のIDを入れる。
