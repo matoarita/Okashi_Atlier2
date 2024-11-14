05ab09c840071df387c29ff137146588a4c51e98
ShogoArita <aritashogo@gmail.com>
2024-11-14 14:08:27 +0900

    241114 あらためて。魔法演出時の効果音を追加

--- Assets/Scripts/meff_sound_obj1.cs
+++ Assets/Scripts/meff_sound_obj1.cs
@@ -9,14 +9,24 @@ public class meff_sound_obj1 : MonoBehaviour {
     // Use this for initialization
     void Start () {
 
-        //サウンドコントローラーの取得
-        sc = GameObject.FindWithTag("SoundController").GetComponent<SoundController>();
+        Init_Setting();
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void OnEnable()
+    {        
+        Init_Setting();
+    }
+
+    void Init_Setting()
+    {
+        //サウンドコントローラーの取得
+        sc = GameObject.FindWithTag("SoundController").GetComponent<SoundController>();
+    }
 
     public void OnSound01()
     {
